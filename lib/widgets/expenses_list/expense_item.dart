b09a0392783f77290ab996e9f582ec81b1efd77f
IRSAGE <sibo.egide98@gmail.com>
2024-07-06 15:28:35 +0200

    Using Icons & Formatting Dates

--- lib/widgets/expenses_list/expense_item.dart
+++ lib/widgets/expenses_list/expense_item.dart
@@ -23,9 +23,9 @@ class ExpenseItem extends StatelessWidget {
                 const Spacer(),
                 Row(
                   children: [
-                    const Icon(Icons.alarm),
+                    Icon(categegoryIcons[expense.category]),
                     const SizedBox(width: 8),
-                    Text(expense.date.toString()),
+                    Text(expense.formattedDate),
                   ],
                 )
               ],
